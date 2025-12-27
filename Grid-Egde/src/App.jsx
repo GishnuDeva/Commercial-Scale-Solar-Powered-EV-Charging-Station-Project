ff0231de2155eb186bd42c66c682b2ec04b56e7f
Pranav-charge <pranav@chargemod.com>
2025-12-27 10:18:05 +0530

    installed vite and tailwind

---
+++ Grid-Egde/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+function App() {
+
+  return (
+    <>
+      <div>
+        <h2 className="mt-6 text-[32px] text-center font-bold">Hello World!</h2>
+       </div>
+    </>
+  )
+}
+
+export default App
